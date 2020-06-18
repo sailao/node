03c75dbd1c6e05956808322d5735baad5d59afdb
teamironowl <teamironowl@gmail.com>
2020-06-18 15:06:54 +0630

    first commit

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 "use strict";
 
+require('dotenv').config();
 const express = require("express");
-const fccTesting = require("./freeCodeCamp/fcctesting.js");
-
 const app = express();
+const mongo = require('mongodb').MongoClient;
+const route = require('./route.js')
+const auth = require('./auth.js')
 
-fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
-app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
-});
+mongo.connect(process.env.DATABASE, (err, db) => {
+    if(err){
+        console.log('Database error: ' + err);
+        return;
+    }
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+    auth(app, db);
+
+    route(app, db);
+
+    app.listen(process.env.PORT || 3000, () => {
+        console.log("Listening on port " + process.env.PORT);
+    })
 });
